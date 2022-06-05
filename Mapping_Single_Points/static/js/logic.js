122346ae402f58e5f94aa954a21071c81f1df277
John Morrissey <morrissey466@gmail.com>
2022-06-05 11:35:24 -0500

    adding

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -63,27 +63,3 @@ let line = [
 L.polyline(line, {
     color: "red"
   }).addTo(map);
-
-  "Geometry": {"type":"FeatureCollection","features":
-[
-  {
-    "type":"Feature","properties":{
-"airline":"AA","airline_id":"24","src":"LAX","dst":"ABQ","dst_id":"4019","stops":"0","equipment":"CRJ CR7"},"geometry":{
-"type":"LineString",
-"coordinates":[[-118.4079971,33.94250107],[-106.609001,35.040199]]}
-},
-{
-"type":"Feature","properties":{
-"airline":"AA","airline_id":"24","src":"LAX","src_id":"3484","dst":"ANC","dst_id":"3774","codeshare":"Y","stops":"0","equipment":"737"},"geometry":{
-"type":"LineString",
-"coordinates":[[-118.4079971,33.94250107],[-149.99600219726562,61.174400329589844]]}
-  },
-{
-  "type":"Feature","properties":{
-"airline":"AA","airline_id":"24","src":"LAX","src_id":"3484","dst":"AUS","dst_id":"3673","codeshare":"","stops":"0","equipment":"M83 738"},"geometry":{
-"type":"LineString",
-"coordinates":[[-118.4079971,33.94250107],[-97.6698989868164,30.194499969482422]]}
-  }
-]
-
-}
